39b2d76337c2a689af6aa1a98a7746ad0d545dc8
Siddhu054 <siddharthaballa143@gmail>
2025-03-02 22:54:11 +0530

    Updated project files

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // ... existing config ...
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn' // changes error to warning
+  }
+} 
---
+++ src/types/declarations.d.ts
@@ -0,0 +1,3 @@
+declare module 'mongoose' {
+  export * from '@types/mongoose';
+} 
