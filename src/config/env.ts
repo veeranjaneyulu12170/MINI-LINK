79fe38249f184fb51c5d897a1ffe130f210764c3
Siddhu054 <siddharthaballa143@gmail>
2025-03-04 20:49:53 +0530

    Updated file(s) with latest changes

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: import.meta.env.VITE_API_URL || 'https://minilink1.onrender.com'
+  API_URL: import.meta.env.VITE_API_URL || 'https://mini-link-ddch.onrender.com'
 };
 
 export default config; 
